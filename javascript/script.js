94d03a1089d6b6b5059b28ca7a3812ac1cf15ed8
GyanRibeiro <gyanribeiro29@gmail.com>
2023-10-14 16:41:18 -0300

    ✨ feat: Carrinho de compras funcional no mobile

--- javascript/script.js
+++ javascript/script.js
@@ -104,7 +104,19 @@ c('.pizzaInfo--addButton').addEventListener('click', () =>{
 
 let somaTotal = 0;
 
+c('.menu-openner').addEventListener('click', () => {
+    if(cart.length > 0) {
+        c('aside').style.left = 0;
+    }
+})
+
+c('.menu-closer').addEventListener('click', () => {
+    c('aside').style.left = '100vw'
+})
+
 function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length;
+
     if(cart.length > 0) {
         c('aside').classList.add('show')
         c('.cart').innerHTML = ''
@@ -174,6 +186,7 @@ function updateCart() {
         c('.preco-desconto').innerHTML = desconto.toFixed(2);
         c('.total-compra').innerHTML = total.toFixed(2);
     } else {
-        c('aside').classList.remove('show')
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw'
     }
 }
